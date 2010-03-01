ccb3857c9e08b67b77182e5ba32186c9a11a4002
nicolas.frenay <nicolas.frenay@930f410e-faa6-11de-9656-bb0dda18cfc1>
2010-03-01 06:32:53 +0000

    Fixed server crashing on wrong keypresses. Doesn't solve the problem but at least it won't stop working. Problematic keypresses are shown on the console (System.out).

--- RemoteDroidServer/src/OSCWorld.java
+++ RemoteDroidServer/src/OSCWorld.java
@@ -1,22 +1,33 @@
-import com.illposed.osc.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Label;
+import java.awt.MouseInfo;
+import java.awt.PointerInfo;
 import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.net.InetAddress;
 
-import jsera.util.*;
+import jsera.util.World;
+
+import com.illposed.osc.OSCListener;
+import com.illposed.osc.OSCMessage;
+import com.illposed.osc.OSCPort;
+import com.illposed.osc.OSCPortIn;
 
 /**
  * 
  * @author jsera
- *
+ * 
  * 
  */
 
 public class OSCWorld extends World {
 	//
 	private static final float sensitivity = 1.6f;
-	
+
 	//
 	private OSCPortIn receiver;
 	//
@@ -33,13 +44,12 @@ public class OSCWorld extends World {
 	private Label lbDebug;
 	//
 	private int scrollMod = -1;
-	
+
 	public OSCWorld() {
 		super();
-		
-		
+
 	}
-	
+
 	public void onEnter() {
 		try {
 			this.robot = new Robot();
@@ -57,7 +67,8 @@ public class OSCWorld extends World {
 				public void acceptMessage(java.util.Date time, OSCMessage message) {
 					Object[] args = message.getArguments();
 					if (args.length == 3) {
-						mouseEvent(Integer.parseInt(args[0].toString()), Float.parseFloat(args[1].toString()), Float.parseFloat(args[2].toString()));
+						mouseEvent(Integer.parseInt(args[0].toString()), Float.parseFloat(args[1]
+								.toString()), Float.parseFloat(args[2].toString()));
 					}
 				}
 			};
@@ -87,7 +98,8 @@ public class OSCWorld extends World {
 				public void acceptMessage(java.util.Date time, OSCMessage message) {
 					Object[] args = message.getArguments();
 					if (args.length == 3) {
-						keyboardEvent(Integer.parseInt(args[0].toString()), Integer.parseInt(args[1].toString()), args[2].toString());
+						keyboardEvent(Integer.parseInt(args[0].toString()), Integer
+								.parseInt(args[1].toString()), args[2].toString());
 					}
 				}
 			};
@@ -107,7 +119,10 @@ public class OSCWorld extends World {
 				public void acceptMessage(java.util.Date time, OSCMessage message) {
 					Object[] args = message.getArguments();
 					if (args.length == 6) {
-						orientEvent(Float.parseFloat(args[0].toString()), Float.parseFloat(args[1].toString()), Float.parseFloat(args[2].toString()), Float.parseFloat(args[3].toString()), Float.parseFloat(args[4].toString()), Float.parseFloat(args[5].toString()));
+						orientEvent(Float.parseFloat(args[0].toString()), Float.parseFloat(args[1]
+								.toString()), Float.parseFloat(args[2].toString()), Float
+								.parseFloat(args[3].toString()), Float.parseFloat(args[4]
+								.toString()), Float.parseFloat(args[5].toString()));
 					}
 				}
 			};
@@ -119,23 +134,23 @@ public class OSCWorld extends World {
 				public void keyReleased(KeyEvent e) {
 					nativeKeyEvent(e);
 				}
-				
+
 				public void keyPressed(KeyEvent e) {
-					
+
 				}
-				
+
 				public void keyTyped(KeyEvent e) {
-					
+
 				}
 			});
 			//
 			this.gDevices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
 			int l = this.gDevices.length;
 			this.gBounds = new Rectangle[l];
-			for (int i=0;i<l;++i) {
+			for (int i = 0; i < l; ++i) {
 				this.gBounds[i] = this.gDevices[i].getDefaultConfiguration().getBounds();
 			}
-			//GlobalData.oBase.pause();
+			// GlobalData.oBase.pause();
 			this.initUI();
 			//
 			if (System.getProperty("os.name").compareToIgnoreCase("Mac OS X") == 0) {
@@ -143,26 +158,26 @@ public class OSCWorld extends World {
 				this.scrollMod = 1;
 			}
 		} catch (Exception ex) {
-			
+
 		}
 	}
-	
+
 	// check keyboard events
-	
+
 	private void nativeKeyEvent(KeyEvent ev) {
 	}
-	
+
 	//
-	
+
 	private void mouseEvent(int type, float xOffset, float yOffset) {
 		if (type == 2) {
 			PointerInfo info = MouseInfo.getPointerInfo();
 			if (info != null) {
 				java.awt.Point p = info.getLocation();
-				p.x += (int)(xOffset * sensitivity);
-				p.y += (int)(yOffset * sensitivity);
+				p.x += (int) (xOffset * sensitivity);
+				p.y += (int) (yOffset * sensitivity);
 				int l = this.gBounds.length;
-				for (int i=0;i<l;++i) {
+				for (int i = 0; i < l; ++i) {
 					if (this.gBounds[i].contains(p)) {
 						this.robot.mouseMove(p.x, p.y);
 						break;
@@ -171,7 +186,7 @@ public class OSCWorld extends World {
 			}
 		}
 	}
-	
+
 	private void buttonEvent(int type, int button) {
 		if (button == 0) {
 			button = InputEvent.BUTTON1_MASK;
@@ -179,116 +194,148 @@ public class OSCWorld extends World {
 			button = InputEvent.BUTTON3_MASK;
 		}
 		switch (type) {
-			case 0:
-				//
-				this.robot.mousePress(button);
-				this.robot.waitForIdle();
-				break;
-			case 1:
-				//
-				this.robot.mouseRelease(button);
-				this.robot.waitForIdle();
-				break;
+		case 0:
+			//
+			this.robot.mousePress(button);
+			this.robot.waitForIdle();
+			break;
+		case 1:
+			//
+			this.robot.mouseRelease(button);
+			this.robot.waitForIdle();
+			break;
 		}
 	}
-	
+
 	private void scrollEvent(int dir) {
 		this.robot.mouseWheel(dir * this.scrollMod);
 	}
-	
+
 	private void keyboardEvent(int type, int keycode, String value) {
 		//
 		KeyCodeData data;
 		switch (type) {
-			case 0:
-				// key down
-				
-				// check if it isn't a mouse click (trackpad "enter" = left button)
-				if (this.translator.isLeftClick(keycode)) {
-					buttonEvent(0, 0);
-					return;
-				}
-				
-				// it's not a mouse event, treat as key
-				if (this.translator.isModifier(keycode)) {
-					this.modified = true;
-				}
-				if (this.translator.isShift(keycode)) {
-					this.shifted = true;
-					this.robot.keyPress(KeyEvent.VK_SHIFT);
-				}
-				if (this.translator.isCtrl(keycode)) {
-					this.robot.keyPress(KeyEvent.VK_CONTROL);
-				}
-				data = (KeyCodeData)translator.codes.get(new Integer(keycode));
-				if (data != null) {
-					if (this.modified) {
-						if (data.shifted && !this.shifted) {
-							this.robot.keyPress(KeyEvent.VK_SHIFT);
-						}
-						if (!data.shifted && this.shifted) {
-							this.robot.keyRelease(KeyEvent.VK_SHIFT);
-						}
-						//
-						if (data.modifiedcode != -1) this.robot.keyPress(data.modifiedcode);
-						//
-						if (data.shifted && !this.shifted) {
-							this.robot.keyRelease(KeyEvent.VK_SHIFT);
-						}
-						if (!data.shifted && this.shifted) {
-							this.robot.keyPress(KeyEvent.VK_SHIFT);
-						}
-					} else {
-						this.robot.keyPress(data.localcode);
+		case 0:
+			// key down
+
+			// check if it isn't a mouse click (trackpad "enter" = left button)
+			if (this.translator.isLeftClick(keycode)) {
+				buttonEvent(0, 0);
+				return;
+			}
+
+			// it's not a mouse event, treat as key
+			if (this.translator.isModifier(keycode)) {
+				this.modified = true;
+			}
+			if (this.translator.isShift(keycode)) {
+				this.shifted = true;
+				this.keyPress(KeyEvent.VK_SHIFT);
+			}
+			if (this.translator.isCtrl(keycode)) {
+				this.keyPress(KeyEvent.VK_CONTROL);
+			}
+			data = (KeyCodeData) translator.codes.get(new Integer(keycode));
+			if (data != null) {
+				if (this.modified) {
+					if (data.shifted && !this.shifted) {
+						this.keyPress(KeyEvent.VK_SHIFT);
+					}
+					if (!data.shifted && this.shifted) {
+						this.keyRelease(KeyEvent.VK_SHIFT);
+					}
+					//
+					if (data.modifiedcode != -1)
+						this.keyPress(data.modifiedcode);
+					//
+					if (data.shifted && !this.shifted) {
+						this.keyRelease(KeyEvent.VK_SHIFT);
+					}
+					if (!data.shifted && this.shifted) {
+						this.keyPress(KeyEvent.VK_SHIFT);
+					}
+				} else {
+					try {
+						this.keyPress(data.localcode);
+					} catch (IllegalArgumentException e) {
+						System.out.println("Invalid key code: " + data.localcode);
 					}
 				}
-				break;
-			case 1:
-				// key up
-				
-				// check if it isn't a mouse click (trackpad "enter" = left button)
-				if (this.translator.isLeftClick(keycode)) {
-					buttonEvent(1, 0);
-					return;
-				}
-				
-				// it's not a mouse event, treat as key
-				if (this.translator.isModifier(keycode)) {
-					this.modified = false;
-				}
-				if (this.translator.isShift(keycode)) {
-					this.shifted = false;
-					this.robot.keyRelease(KeyEvent.VK_SHIFT);
-				}
-				if (this.translator.isCtrl(keycode)) {
-					this.robot.keyRelease(KeyEvent.VK_CONTROL);
-				}
-				data = (KeyCodeData)translator.codes.get(new Integer(keycode));
-				if (data != null) {
-					if (this.modified) {
-						if (data.shifted && !this.shifted) {
-							this.robot.keyPress(KeyEvent.VK_SHIFT);
-						}
-						if (!data.shifted && this.shifted) {
-							this.robot.keyRelease(KeyEvent.VK_SHIFT);
-						}
-						//
-						if (data.modifiedcode != -1) this.robot.keyRelease(data.modifiedcode);
-						//
-						if (data.shifted && !this.shifted) {
-							this.robot.keyRelease(KeyEvent.VK_SHIFT);
-						}
-						if (!data.shifted && this.shifted) {
-							this.robot.keyPress(KeyEvent.VK_SHIFT);
-						}
-					} else {
-						this.robot.keyRelease(data.localcode);
+			}
+			break;
+		case 1:
+			// key up
+
+			// check if it isn't a mouse click (trackpad "enter" = left button)
+			if (this.translator.isLeftClick(keycode)) {
+				buttonEvent(1, 0);
+				return;
+			}
+
+			// it's not a mouse event, treat as key
+			if (this.translator.isModifier(keycode)) {
+				this.modified = false;
+			}
+			if (this.translator.isShift(keycode)) {
+				this.shifted = false;
+				keyRelease(KeyEvent.VK_SHIFT);
+			}
+			if (this.translator.isCtrl(keycode)) {
+				keyRelease(KeyEvent.VK_CONTROL);
+			}
+			data = (KeyCodeData) translator.codes.get(new Integer(keycode));
+			if (data != null) {
+				if (this.modified) {
+					if (data.shifted && !this.shifted) {
+						this.keyPress(KeyEvent.VK_SHIFT);
 					}
+					if (!data.shifted && this.shifted) {
+						this.keyRelease(KeyEvent.VK_SHIFT);
+					}
+					//
+					if (data.modifiedcode != -1)
+						this.keyRelease(data.modifiedcode);
+					//
+					if (data.shifted && !this.shifted) {
+						this.keyRelease(KeyEvent.VK_SHIFT);
+					}
+					if (!data.shifted && this.shifted) {
+						this.keyPress(KeyEvent.VK_SHIFT);
+					}
+				} else {
+					this.keyRelease(data.localcode);
 				}
-				break;
+			}
+			break;
 		}
 	}
-	
+
+	/**
+	 * Calls the robot method and catches exceptions due to invalid key.
+	 * 
+	 * @param localcode
+	 */
+	private void keyPress(int localcode) {
+		try {
+			this.robot.keyPress(localcode);
+		} catch (IllegalArgumentException e) {
+			System.out.println("Invalid keyPress code: " + localcode);
+		}
+	}
+
+	/**
+	 * Calls the robot method and catches exceptions due to invalid key.
+	 * 
+	 * @param localcode
+	 */
+	private void keyRelease(int localcode) {
+		try {
+			this.robot.keyRelease(localcode);
+		} catch (IllegalArgumentException e) {
+			System.out.println("Invalid keyRelease code: " + localcode);
+		}
+	}
+
 	private void orientEvent(float z, float x, float y, float rawz, float rawx, float rawy) {
 		StringBuilder builder = new StringBuilder();
 		this.addValue(builder, "z", z);
@@ -298,28 +345,28 @@ public class OSCWorld extends World {
 		this.addValue(builder, "rawx", rawx);
 		this.addValue(builder, "rawy", rawy);
 		//
-		double len = Math.sqrt(x*x+y*y+z*z);
-		this.addValue(builder, "len", (float)len);
+		double len = Math.sqrt(x * x + y * y + z * z);
+		this.addValue(builder, "len", (float) len);
 		//
 		this.lbDebug.setText(builder.toString());
 	}
-	
+
 	// UI
-	
+
 	private void initUI() {
-		
+
 	}
-	
+
 	private void addValue(StringBuilder builder, String name, float value) {
 		builder.append(name);
 		builder.append(": ");
 		builder.append(value);
 		builder.append("\n");
 	}
-	
+
 	//
-	
+
 	public void update(float elapsed) {
-		
+
 	}
 }
